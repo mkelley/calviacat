e473eecb717def04807915c8b5dc950962e61107
Michael S. P. Kelley <msk@astro.umd.edu>
2024-03-08 09:48:25 -0500

    Migrate to pyproject.toml

--- calviacat/__init__.py
+++ calviacat/__init__.py
@@ -1,5 +1,13 @@
+# Licensed with the MIT License, see LICENSE for details
+from importlib.metadata import version as _version, PackageNotFoundError
+
 from .catalog import *
 from .panstarrs1 import PanSTARRS1
 from .skymapper import SkyMapper
 from .refcat2 import RefCat2
 from .gaia import Gaia
+
+try:
+    __version__ = _version(__name__)
+except PackageNotFoundError:
+    pass
